b00a0236986880cbe5cb69c174a5cdba877d292c
duzhg56980 <guokey1996@gmail.com>
2024-02-19 11:55:18 +0800

    init commit

---
+++ 272463-1.2.22/FunctionUse1427.java
@@ -0,0 +1,6 @@
+import com.alibaba.fastjson.TypeReference;
+public class FunctionUse1427 {
+public void funcUse() {
+TypeReference typereference = new TypeReference();
+}
+}
---
+++ 272463-1.2.22/FunctionUse1460.java
@@ -0,0 +1,6 @@
+import com.alibaba.fastjson.util.FieldInfo;
+public class FunctionUse1460 {
+public void funcUse() {
+FieldInfo fieldinfo = new FieldInfo();
+}
+}
